222f498ad58555b13da2c5cf2dd1ed4f1b0cc364
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-06-17 03:29:03 +0300

    Deleting Author

--- BookStroe/Controllers/AuthorController.cs
+++ BookStroe/Controllers/AuthorController.cs
@@ -77,16 +77,18 @@ namespace BookStroe.Controllers
         // GET: AuthorController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var author =authorRepository.FindById(id);  
+            return View(author);
         }
 
         // POST: AuthorController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Author author)
         {
             try
             {
+                authorRepository.DeleteById(id);
                 return RedirectToAction(nameof(Index));
             }
             catch
