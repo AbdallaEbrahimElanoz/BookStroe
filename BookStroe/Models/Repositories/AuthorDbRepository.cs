de3d9e6b6775721d1f1270e84559110ad5a999e8
AbdallaEbrahimElanoz <anaabdoa319@gmail.com>
2023-06-20 17:58:46 +0300

    Entity Framework Core Repositories Db book and author

---
+++ BookStroe/Models/Repositories/AuthorDbRepository.cs
@@ -0,0 +1,85 @@
+﻿using BookStore.Models.Repositories;
+using BookStore.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace BookStroe.Models.Repositories
+{
+    public class AuthorDbRepository : IBookStoreRepository<Author>
+    {
+        BookstoreDbContext db;
+        public AuthorDbRepository(BookstoreDbContext _db)
+        {
+            db = _db;
+        }
+        public void Add(Author entity)
+        {
+            db.Authors.Add(entity);
+        }
+
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            var author = Find(id);
+            db.Authors.Remove(author);
+        }
+
+
+        public Author Find(int id)
+        {
+            var author = db.Authors.SingleOrDefault(x => x.Id == id);
+            return author;
+        }
+
+        public Author Find(string authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Author> List()
+        {
+            return Authors;
+        }
+
+        public void Update(int id, Author newAuthor)
+        {
+            var author = Find(id);
+            author.FullName = newAuthor.FullName;
+        }
+
+        public void Update(Author entity, int bookId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int bookId, Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ViewModel(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void viewModel(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookStroe/Models/Repositories/BookDbRepository.cs
@@ -0,0 +1,92 @@
+﻿using BookStore.Models.Repositories;
+using BookStore.Models;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace BookStroe.Models.Repositories
+{
+    public class BookDbRepository : IBookStoreRepository<Book>
+    {
+        BookstoreDbContext db;
+        public BookDbRepository(BookstoreDbContext db)
+        {
+            this.db = db;
+        }
+        public void Add(Book entity)
+        {
+            db.Books.Add(entity);
+            db.SaveChanges();
+        }
+        public Book Find(int id)
+        {
+            var book = db.Books.SingleOrDefault(x => x.Id == id);
+            return book;
+        }
+
+
+        public void Delete(int id)
+        {
+            var book = Find(id);
+            db.Books.Remove(book);
+            db.SaveChanges();
+        }
+
+
+        public IList<Book> List()
+        {
+            return db.Books.ToList();
+        }
+
+        public void UpdateById(Book entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Book newBook)
+        {
+            db.Books.Update(newBook);
+            db.SaveChanges();
+
+
+        }
+
+        public void Update(int id, Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Find(string authorId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public void Update(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ViewModel(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void viewModel(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Book entity, int bookId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
